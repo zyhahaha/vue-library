45945a3adc89cc7f74d9a47976a529ff6a79020e
zhaoyang <18355403288@163.com>
2019-05-21 17:53:54 +0800

    update

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  publicPath: './',
+  // devServer: {
+  //   port: 8888, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:8088',
+  //       changeOrigin: true,
+  //       pathRewrite: {
+  //         '^/api': '/'
+  //       }
+  //     }
+  //   }
+  // }
+};
